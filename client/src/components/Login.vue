c44a8d2efd2a9ca1e7cc4ea887350e1194bc3b95
Alen <alkuca@gmail.com>
2020-01-04 18:33:46 +0100

    Login component empty

---
+++ client/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login-page">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+    .login-page{
+        width:100%;
+        height:100vh;
+        background:#2E34CC;
+    }
+</style>
