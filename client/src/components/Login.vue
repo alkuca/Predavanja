4aaa5ebe29528f57b468476d06df02c75f5ea3af
Alen <alkuca@gmail.com>
2020-01-11 18:41:13 +0100

    login form container

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="login-page">
-
+        <div class="login-form-container">
+            <div class="logo-container">
+                <img src="../assets/logo.svg" alt="logo"/>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,5 +19,28 @@
         width:100%;
         height:100vh;
         background:#2E34CC;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .login-form-container{
+        width:370px;
+        height:370px;
+        background:white;
+        border-radius:5px;
+        box-shadow: 0 0 11px rgba(0, 0, 0, 0.40);
+        position:relative;
+    }
+    .logo-container{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        height: 110px;
+    }
+    .logo-container img{
+        width:200px;
     }
 </style>
