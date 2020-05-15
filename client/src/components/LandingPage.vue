a4930d55566612accf723d9f66304746aafe2e9c
Alen <alkuca@gmail.com>
2020-05-15 13:52:11 +0200

    landing page fix

--- client/src/components/LandingPage.vue
+++ client/src/components/LandingPage.vue
@@ -2,14 +2,19 @@
     <div class="landing-page">
         <nav>
             <h1>Lorem</h1>
+            <div class="nav-links">
+                <p class="register">Register</p>
+                <p class="login">Login</p>
+                <div class="underline"/>
+            </div>
         </nav>
         <div class="main-container">
             <section class="section-left">
                 <p class="small-title">LOREM IPSUM DOLOR SIT AMET</p>
                 <h1 class="main-title">Lorem ipsum dolor sit amet,consectetur.</h1>
                 <p class="title-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vivamus ut dolor purus. Lorem ipsum dolor sit amet consectetur adipiscing.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sem eu tellus gravida egestas maximus nec felis.
+                    Nunc suscipit, eros et finibus placerat, nisl neque interdum dolor, iaculis fermentum nisi urna a neque.
                 </p>
                 <div class="start-button">
                     <p>Get Started</p>
@@ -41,37 +46,57 @@
         width:100%;
         padding:0;
         margin:0;
+        height: calc(100% - 69px);
         display: flex;
     }
     nav{
         z-index: 2;
         position: relative;
+        display: flex;
+        justify-content: space-between;
+        padding: 0 100px;
+        align-items: center;
+        border-bottom:1px solid rgba(0, 0, 0, 0.20)
     }
-    nav h1{
-        padding:30px 0 0 50px;
+    .nav-links{
+        display: flex;
+        position: relative;
+    }
+    .nav-links p{
+        font-weight: bold;
+        color:#303030;
+        padding: 25px 30px;
         margin: 0;
+
+        cursor: pointer;
+    }
+    .nav-links p:hover{
+
     }
     .section-left{
-        width:calc(40% - 50px);
-        margin-left:50px;
-        margin-top: 9%;
-        margin-bottom: 9%;
+        width:calc(40% - 100px);
+        margin-left:100px;
         position: relative;
         z-index: 2;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
     }
     .small-title{
         color:#8E93FF;
         font-weight: bold;
         margin: 5px 0;
+        font-size: 22px;
     }
     .main-title{
         font-weight: bold;
-        font-size: 40px;
+        font-size: 60px;
         margin: 10px 0;
         color:#303030;
     }
     .title-text{
         color: #404040;
+        font-size: 18px;
     }
     .section-right{
         width: calc(60% - 50px);
@@ -90,7 +115,7 @@
         position: absolute;
         width: 90px;
         right:0;
-        bottom:-61px;
+        bottom:150px;
         z-index: 2;
     }
     .blue-background-section{
@@ -98,9 +123,77 @@
         top:0;
         left:0;
         background: rgba(248, 248, 255, 0.70);
-        width: calc(40% + 50px);
-        height: 475px;
+        width: calc(40% + 80px);
+        height: calc(55% + 200px);
         z-index: 1;
+    }
+    .start-button p {
+        text-decoration: none;
+        transition:0.2s;
+        margin:0;
+    }
+    .start-button {
+        position: relative;
+        padding: 7px 5px;
+        text-align: center;
+        cursor: pointer;
+        overflow: hidden;
+        text-decoration: none;
+        width:165px;
+        font-weight: bold;
+        font-size: 26px;
+        color:#4A50D9;
+        border-left: 5px solid #4A50D9;
+        transition: 0.2s;
+        z-index: 10;
+        margin-top:20px;
+    }
+    .start-button:hover p {
+        color:white;
+    }
+    .start-button:hover {
+        transition: 0.2s;
+        width:180px;
+    }
+    .start-button .mask {
+        position: absolute;
+        width: 0;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background: #4A50D9;
+        z-index: -1;
+        transition: 0.2s;
+    }
+    .start-button:hover .mask {
+        background: #4A50D9;
+        width:100%;
+        transition:0.2s;
+        z-index: -1;
+    }
+
+    @media screen and (max-width: 1400px) {
+        .small-title{
+            font-size: 18px;
+        }
+        .main-title{
+            font-size: 40px;
+        }
+        .title-text{
+            font-size: 16px;
+        }
+        .start-button {
+            width:140px;
+            font-size: 22px;
+            margin-top:5px;
+        }
+        nav{
+            padding: 0 50px;
+        }
+        .section-left{
+            width:calc(40% - 50px);
+            margin-left:50px;
+        }
     }
 
     @media screen and (max-width: 1140px) {
@@ -114,7 +207,6 @@
             font-size: 30px;
         }
         .title-text{
-            color:#1C1C1C;
             font-size: 13px;
         }
         .landing-image{
@@ -124,12 +216,6 @@
             width: 70px;
             right:0;
             bottom:-50px;
-        }
-    }
-    @media screen and (max-width: 1140px) {
-        .landing-image{
-            width:70%;
-            margin-left:100px;
         }
     }
     @media screen and (max-width: 850px) {
@@ -154,9 +240,8 @@
             flex-direction: column;
             align-content: center;
         }
-        nav h1{
-            padding:20px 0 0 25px;
-            margin: 0;
+        nav{
+            padding: 0 25px;
         }
         .section-left{
             width: calc(100% - 50px);
@@ -193,48 +278,27 @@
         }
     }
 
-
-    .start-button p {
-        text-decoration: none;
-        transition:0.2s;
-        margin:0;
-    }
-    .start-button {
-        position: relative;
-        padding: 7px 5px;
-        text-align: center;
-        cursor: pointer;
-        overflow: hidden;
-        text-decoration: none;
-        width:140px;
-        font-weight: bold;
-        font-size: 22px;
-        color:#4A50D9;
-        border-left: 4px solid #4A50D9;
-        transition: 0.2s;
-        z-index: 10;
-    }
-    .start-button:hover p {
-        color:white;
-    }
-    .start-button:hover {
-        transition: 0.2s;
-        width:180px;
-    }
-    .start-button .mask {
+    .underline{
         position: absolute;
-        width: 0;
-        height: 100%;
-        top: 0;
-        left: 0;
-        background: #4A50D9;
-        z-index: -1;
-        transition: 0.2s;
+        height:3px;
+        background: transparent;
+        bottom:0;
+        width:100px;
+        left:12px;
+        transition: left 0.2s, width 0.2s, background 0.1s;
+        border-radius: 10px 10px 0 0;
+
     }
-    .start-button:hover .mask {
-        background: #4A50D9;
-        width:100%;
-        transition:0.2s;
-        z-index: -1;
+    .login:hover ~ .underline{
+        left:133px;
+        width:85px;
+        transition: left 0.2s, width 0.2s;
+        background: #46497E;
+    }
+    .register:hover ~ .underline{
+        width:100px;
+        background: #46497E;
+        left:12px;
+        transition: left 0.2s, width 0.2s;
     }
 </style>
